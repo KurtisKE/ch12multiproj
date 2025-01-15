e4cec0960cadb6afd4fdf93cdfcbe64cd00ceaec
Kurtis Evans <kurtis.evans@students.snow.edu>
2025-01-15 09:52:04 -0700

    empty for now

---
+++ ch12.ConsoleUI/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ch12.Logic;
+
+Console.WriteLine("Hello, World!");
+
+var result = Class1.Add(1,5);
+Console.WriteLine(result);
---
+++ ch12.Logic/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ch12.Logic;
+
+public static class Class1
+{
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
