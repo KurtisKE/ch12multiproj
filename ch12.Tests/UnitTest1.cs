06210fc47f326dc8b367ce7bca970386a6600efc
Kurtis Evans <kurtis.evans@students.snow.edu>
2025-01-15 10:00:10 -0700

    added tests

---
+++ ch12.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using ch12.Logic;
+
+namespace ch12.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = Class1.Add(1,5);
+        Assert.Equal(8,result);
+    }
+}
